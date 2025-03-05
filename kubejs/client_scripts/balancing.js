681e7596a9a5c9bce667239e979ab28b7bbf61a7
Ozoku <19756856+ozokuz@users.noreply.github.com>
2025-03-05 17:40:10 +0200

    balance mining gadgets
    removed fortune and voiding upgrades

---
+++ kubejs/client_scripts/balancing.js
@@ -0,0 +1,6 @@
+RecipeViewerEvents.removeEntries('item', (event) => {
+    event.remove("mininggadgets:upgrade_void_junk");
+    event.remove("mininggadgets:upgrade_fortune_1");
+    event.remove("mininggadgets:upgrade_fortune_2");
+    event.remove("mininggadgets:upgrade_fortune_3");
+});
